f79f45c2770de5080a07568999eca47c9e34b20c
jsahrmann <jsahrmann@mednet.ucla.edu>
2023-08-20 13:01:15 -0700

    Import stats package

---
+++ R/ReplicateModifiedPoisson-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats coef
+#' @importFrom stats confint.default
+#' @importFrom stats poisson
+#' @importFrom stats rbinom
+#' @importFrom stats runif
+## usethis namespace: end
+NULL
